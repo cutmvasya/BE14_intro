584494614ddb9bc8809dfe4f10f070482493100a
cutmvasya <meutia.vasya@gmail.com>
2021-08-22 17:45:56 +0700

    Tugas pertama

---
+++ first.js
@@ -0,0 +1,12 @@
+let teks = "Halo";
+var nama = "Vasya";
+console.log(`${teks} ${nama}`);
+
+const kelas = 1;
+console.log(`Kelas: ${kelas}`);
+
+const array = [nama, kelas, {nama: nama, kelas: kelas}];
+console.log(array);
+
+const objek = {kelas: ["frontend", "backend", "mobile"]};
+console.log(objek.kelas[2]);
